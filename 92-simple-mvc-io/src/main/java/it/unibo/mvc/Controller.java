d5c5878efb53785d058f87bea6c0a36c060b6e24
David Andrei Orosanu <davidandrei.orosanu@studio.unibo.it>
2025-11-18 23:04:08 +0100

    just starting implementation

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,34 @@
 package it.unibo.mvc;
 
+import java.io.File;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
 
+    private File fileName;
+
+    public void setFile(File fileName) {
+        if (fileName == null) {
+            throw new IllegalArgumentException();
+        }
+        this.fileName = fileName;
+    }
+
+    public File getFile() {
+        return this.fileName;
+    }
+
+    public String getPath() {
+        if (fileName == null) {
+            throw new IllegalStateException();
+        }
+        return fileName.getPath();
+    }
+
+    public File saveString(String str) {
+        return fileName;
+    }
 }
+
